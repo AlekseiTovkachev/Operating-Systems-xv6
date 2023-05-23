3e933c4d0cf612a5750f4d66dc758aabfb6aa3d8
AlekseiTovkachev <atovkachev@gmail.com>
2023-05-23 17:14:47 +0000

    patch applied

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -170,7 +170,7 @@ bad:
 }
 
 // Is the directory dp empty except for "." and ".." ?
-static int
+int
 isdirempty(struct inode *dp)
 {
   int off;
@@ -242,7 +242,7 @@ bad:
   return -1;
 }
 
-static struct inode*
+struct inode*
 create(char *path, short type, short major, short minor)
 {
   struct inode *ip, *dp;
